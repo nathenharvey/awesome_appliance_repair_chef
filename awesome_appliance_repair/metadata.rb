1d11b50f64cb0be0ab390af275760ea047ec985d
nathenharvey <nharvey@opscode.com>
2014-07-08 21:49:30 -0400

    Scaffold awesome_appliance_repair cookbook
        chef generate cookbook awesome_appliance_repair

---
+++ awesome_appliance_repair/metadata.rb
@@ -0,0 +1,8 @@
+name             'awesome_appliance_repair'
+maintainer       ''
+maintainer_email ''
+license          ''
+description      'Installs/Configures awesome_appliance_repair'
+long_description 'Installs/Configures awesome_appliance_repair'
+version          '0.1.0'
+
---
+++ awesome_appliance_repair/recipes/default.rb
@@ -0,0 +1,8 @@
+#
+# Cookbook Name:: awesome_appliance_repair
+# Recipe:: default
+#
+# Copyright (C) 2014 
+#
+# 
+#
