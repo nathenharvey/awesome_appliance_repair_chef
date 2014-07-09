b3b01c2ca879422e986bdc09e2ba7265dffc4d3a
nathenharvey <nharvey@opscode.com>
2014-07-08 22:09:58 -0400

    Add complete metadata details

--- cookbooks/awesome_appliance_repair/metadata.rb
+++ cookbooks/awesome_appliance_repair/metadata.rb
@@ -1,8 +1,8 @@
 name             'awesome_appliance_repair'
-maintainer       ''
-maintainer_email ''
-license          ''
-description      'Installs/Configures awesome_appliance_repair'
-long_description 'Installs/Configures awesome_appliance_repair'
+maintainer       'Nathen Harvey'
+maintainer_email 'nharvey@getchef.com'
+license          'Apache 2.0'
+description      'Installs/Configures Awesome Applicance Repair application.'
+long_description 'Installs/Configures Awesome Applicance Repair application.'
 version          '0.1.0'
 
